1415ea7e502def0c8bde29f5b25878b155eda431
lucemery <lucemery316@gmail.com>
2024-05-10 18:56:02 -0500

    Finished lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+const menu = {
+    taco: 5.55,
+    salad: 3.89,
+    beans: 5.99,
+    pizza: 6.50,
+    order: function(orderPlaced) {
+        const orderSplit = orderPlaced.split(' ');
+        let sum = 0;
+
+        for (const item of orderSplit) {
+            sum += this[item];
+     }
+
+     console.log(`Your total is ${sum.toFixed(2)}`);
+   }
+}
+menu.order('taco, salad, pizza');
+// menu.order('pizza pizza pizza');
